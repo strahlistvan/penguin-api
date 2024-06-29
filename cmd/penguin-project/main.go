3535bc32ab9e1d8a14d4de57dbb2315402072207
Stráhl István <strahlistvan0@gmail.com>
2024-06-29 17:03:06 +0200

    feat: sum GET parameters

--- cmd/penguin-project/main.go
+++ cmd/penguin-project/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello Világ!")
+	fmt.Println("Server running on localhost port 8082")
+	runAPI()
+}
+
+func runAPI() {
+	http.HandleFunc("/test", TestHandle)
+	http.ListenAndServe(":8082", nil)
+}
+
+func TestHandle(wr http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(wr, "Hello Világ\n")
+	fmt.Fprintf(wr, "Az összeg %v\n", getSum(req))
+}
+
+func getSum(req *http.Request) int {
+	params := req.URL.Query()
+	a := params.Get("a")
+	b := params.Get("b")
+
+	aNumber, _ := strconv.Atoi(a)
+	bNumber, _ := strconv.Atoi(b)
+
+	return (aNumber + bNumber)
 }
